3b508fe0cbd528d4a86fb63fbca87205cc986b29
Thomas <introt@gmail.com>
2015-09-12 21:41:48 +0200

    edit set record function

--- src/lift_records/db.clj
+++ src/lift_records/db.clj
@@ -4,6 +4,7 @@
   (:require [clojure.java.io :as io]))
 
 (def db-path "~/.lift_records/db.json")
+(def pattern #"(\d+)x(\d+)")
 
 (defn load-db
   []
@@ -15,7 +16,7 @@
 
 (defn save-db
   [db]
-  (spit db-path db)
+  (spit db-path (json/write-str db))
   db)
 
 (defn get-max
@@ -24,16 +25,19 @@
 
 (defn set-new-record
   [db name set reps {w :weight date :date :as record}]
-  (let [updated (assoc-in-with concat (db-keys :records) record)]
-    (assoc-in-with #(if (> %1 %2) %1 %2) updated (db-keys :max) w 1)))
+  (let [updated (assoc-in-with conj db (db-keys :records) record [])]
+    (assoc-in-with #(if (> %1 %2) %1 %2) updated (db-keys :max) w 0)))
 
 (defn assoc-in-with 
-  [f map keys val]
-  (let [old (apply get-in (into [map] [keys]))]
-    (assoc-in map keys (f old val))))
+  [f map keys val default]
+  (if-let [old (apply get-in (into [map] [keys]))]
+    (assoc-in map keys (f old val))
+    (assoc-in map keys (f default val))))
 
 (defmacro db-keys [last]
   (vector 'name 'set 'reps last))
+
+
 
 
 
